6c8b0cc7fc6b630133eb53d782667414881939ee
Jon Pacheco <jPacheco@ipipeline.com>
2022-03-25 10:54:26 +0000

    feat: convert between celsius and farenheit

--- src/lib.rs
+++ src/lib.rs
@@ -3,6 +3,7 @@
 mod calculate_median;
 mod case_insensitive_sort;
 mod check_deadline_reached;
+mod convert_celsius_farenheit;
 mod convert_to_morse_code;
 mod find_unique_items;
 mod print_any_text_type;
